db67efedca99ee6fe6de1bbc556cee9bcf2ae657
Matthew Lipper <mlipper@gmail.com>
2004-01-06 21:03:17 +0000

    Added links to main site page.

---
+++ .rf.sh
@@ -0,0 +1,7 @@
+CVSROOT=:ext:$USER@rubyforge.org:/var/cvs/runt && export CVSROOT
+
+CVS_RSH=ssh && export CVS_RSH
+
+#cvs co runt
+
+#scp index.html $USER@rubyforge.org:/var/www/gforge-projects/runt/index.html
