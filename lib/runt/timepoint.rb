4a90a28d2842b4fa515c9da6a7d6fdcaa514a558
Matthew Lipper <mlipper@gmail.com>
2004-01-26 01:24:04 +0000

    Tick, tock, tick, tock...

--- lib/runt/timepoint.rb
+++ lib/runt/timepoint.rb
@@ -24,7 +24,6 @@ class Date
 				precision = args.shift
 			else
 				return Runt::DatePrecision.day_of_month(*args)
-				#precision = nil
 			end
 			_civil = old_civil(*args)
 			_civil.date_precision = precision
@@ -33,41 +32,10 @@ class Date
 	end
 	
 	class << self; alias_method :new, :civil end
-
-	#~ def + (n)
-		#~ raise TypeError, 'expected numeric' unless n.kind_of?(Numeric) 
-		
-		#~ if(leap?) 
-			#~ days_in_year = 365
-		#~ else
-			#~ days_in_year = 366
-		#~ end
-		
-		#~ case @date_precision
-			#~ when Runt::DatePrecision::YEAR then 
-				#~ return new_self_plus(n){ |n| n = n*days_in_year }	
-			#~ when Runt::DatePrecision::MONTH then 
-				#~ return new_self_plus(n){ |n| n = (n*(days_in_year/12).to_i)} 
-			#~ #Default behaviour already in Date
-			#~ when Runt::DatePrecision::DAY_OF_MONTH then 
-				#~ return new_self_plus(n){ |n| n = n }			
-		#~ end
-	#~ end
-	
-	#~ private
-
-	#~ def new_self_plus(n)		
-		#~ if(block_given?)
-			#~ n=yield(n) 
-		#~ end
-		
-		#~ return Runt::DatePrecision::to_p(self.class.new0(@ajd + n, @of, @sg),@date_precision)
-	#~ end
 end
 
 class DateTime
 
-	attr_accessor :date_precision
 	class << self
 
   	alias_method :old_civil, :civil
@@ -76,7 +44,7 @@ class DateTime
 			if(args[0].instance_of?(Runt::DatePrecision::Precision))
 				precision = args.shift
 			else
-				return Runt::DatePrecision.minute(*args)			
+				return Runt::DatePrecision.second(*args)			
 			end
 		_civil = old_civil(*args)
 		_civil.date_precision = precision
@@ -84,51 +52,61 @@ class DateTime
 		end
 	end
 		
-	class << self; alias_method :new, :civil end
-	
+	class << self; alias_method :new, :civil end	
 end
 
 module Runt
 			
-		class TimePoint < DateTime
-			include DatePrecision
+	class TimePoint < DateTime
+		include DatePrecision
 
-			attr_reader :date_precision
+		attr_reader :date_precision
 			
-			def initialize(*args)
-				super(*args)
+        def initialize(*args)
+        	super(*args)
+        end
+
+        def + (n)
+            raise TypeError, 'expected numeric' unless n.kind_of?(Numeric)
+						
+            if(leap?) 
+                days_in_year = 365
+            else
+                days_in_year = 366
+            end
+						#FIXME!!!!
+            case @date_precision
+                when Runt::DatePrecision::YEAR then 
+                    #~ return new_self_plus(n){ |n| n = n*days_in_year }	
+										return Runt::DatePrecision::to_p(TimePoint::civil(year+n,month,day),@date_precision)
+                when Runt::DatePrecision::MONTH then 
+                    return new_self_plus(n){ |n| n = (n*(days_in_year/12).to_i)} 
+                when Runt::DatePrecision::DAY_OF_MONTH then 
+                    #Default behaviour already in Date
+                    return new_self_plus(n){ |n| n = n }			
+                when Runt::DatePrecision::HOUR_OF_DAY then 
+                    return new_self_plus(n){ |n| n = (n*(1.to_r/24) ) }			
+                when Runt::DatePrecision::MINUTE then 
+                    return new_self_plus(n){ |n| n = (n*(1.to_r/1440) ) }		
+                when Runt::DatePrecision::SECOND then 
+                    return new_self_plus(n){ |n| n = (n*(1.to_r/86400) ) }
+            end
+        end
+        
+		def - (x)
+			case x
+				when Numeric; return self+(-x)
+				#FIXME!!
+				when Date;    return @ajd - x.ajd
 			end
-			
-			def + (n)
-				raise TypeError, 'expected numeric' unless n.kind_of?(Numeric) 
-			if(leap?) 
-				days_in_year = 365
-			else
-				days_in_year = 366
-			end
-			
-			case @date_precision
-				when Runt::DatePrecision::YEAR then 
-					return new_self_plus(n){ |n| n = n*days_in_year }	
-				when Runt::DatePrecision::MONTH then 
-					return new_self_plus(n){ |n| n = (n*(days_in_year/12).to_i)} 
-				when Runt::DatePrecision::DAY_OF_MONTH then 
-				#Default behaviour already in Date
-					return new_self_plus(n){ |n| n = n }			
-				when Runt::DatePrecision::HOUR_OF_DAY then 
-					return new_self_plus(n){ |n| n = (n*(1.to_r/24) ) }			
-				when Runt::DatePrecision::MINUTE then 
-					return new_self_plus(n){ |n| n = (n*(1.to_r/1440) ) }		
-				when Runt::DatePrecision::SECOND then 
-					return new_self_plus(n){ |n| n = (n*(1.to_r/86400) ) }
-			end
-	end
-	def new_self_plus(n)		
-		if(block_given?)
-			n=yield(n) 
+			raise TypeError, 'expected numeric or date'
 		end
 		
-		return Runt::DatePrecision::to_p(self.class.new0(@ajd + n, @of, @sg),@date_precision)
-	end
+		def new_self_plus(n)		
+			if(block_given?)
+				n=yield(n) 
+			end
+			return Runt::DatePrecision::to_p(self.class.new0(@ajd + n, @of, @sg),@date_precision)
 		end
+	end
 end
--- test/timepointtest.rb
+++ test/timepointtest.rb
@@ -12,8 +12,21 @@ require 'runt'
 class TimePointTest < Test::Unit::TestCase
 
 	include Runt
+
+	def setup
+		# 2010 (August - ignored)
+		@year_prec = DatePrecision.year(2010,8)
+		#August, 2004
+		@month_prec = DatePrecision.month(2004,8)
+		#January 25th, 2004 (11:39 am - ignored)
+		@day_prec = DatePrecision.day_of_month(2004,1,25,11,39)
+		#11:59(:04 - ignored), December 31st, 1999
+		@minute_prec = DatePrecision.minute(1999,12,31,23,59,4)
+		#12:00:10 am, March 1st, 2004
+		@second_prec = DatePrecision.second(2004,3,1,0,0,10)
+	end
 	
-	def test_new		
+	def test_new
 		date = TimePoint.new(2004,2,29)
 		assert(!date.date_precision.nil?)
 		date_time = TimePoint.new(2004,2,29,22,13,2)
@@ -21,13 +34,33 @@ class TimePointTest < Test::Unit::TestCase
 	end
 	
 	def test_plus		
-		year_prec = DatePrecision.year(2010,8)
-		#Year precision will truncate month		
-		assert(DatePrecision.year(2022,12)==(year_prec+12))		
-		month_prec = DatePrecision.month(2004,8)
-		assert(DatePrecision.month(2005,1)==(month_prec+6))		
-		#11:59 (:04 - ignored) December 31st, 1999 
-		prec = DatePrecision.minute(1999,12,31,23,59,4)
+		assert(DatePrecision.year(2022,12)==(@year_prec+12))		
+		assert(DatePrecision.month(2005,1)==(@month_prec+6))		
+		assert(DatePrecision.day_of_month(2004,2,1)==(@day_prec+7))				
+		assert(DatePrecision.minute(2000,1,1,0,0)==(@minute_prec+1))				
+		assert(DatePrecision.second(2004,2,29,23,59,59)==(@second_prec-11))				
  	end
 	
+
+	def test_minus		
+		p DatePrecision.year(1998,12).ctime
+		p @year_prec.ctime		
+		temp = (@year_prec-12)
+		assert(DatePrecision.year(1998,12)==(@year_prec-12))		
+		
+		#~ month_prec = DatePrecision.month(2004,8)
+		#~ assert(DatePrecision.month(2005,1)==(month_prec+6))		
+
+		#~ day_prec = DatePrecision.day_of_month(2004,1,25,11,39)
+		#~ assert(DatePrecision.day_of_month(2004,2,1)==(day_prec+7))				
+		
+		#~ minute_prec = DatePrecision.minute(1999,12,31,23,59,4)
+		#~ assert(DatePrecision.minute(2000,1,1,0,0)==(minute_prec+1))				
+		
+		#~ second_prec = DatePrecision.second(2004,3,1,0,0,10)
+		#~ assert(DatePrecision.second(2004,2,29,23,59,59)==(second_prec-11))				
+ 	end
+
+
+
 end
