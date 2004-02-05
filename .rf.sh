2c637db73503673e50a425b205a123f85de23825
Matthew Lipper <mlipper@gmail.com>
2004-02-05 19:21:52 +0000

    Pre-release clean-up.

--- .rf.sh
+++
@@ -1,7 +0,0 @@
-CVSROOT=:ext:$USER@rubyforge.org:/var/cvs/runt && export CVSROOT
-
-CVS_RSH=ssh && export CVS_RSH
-
-#cvs co runt
-
-#scp index.html $USER@rubyforge.org:/var/www/gforge-projects/runt/index.html
