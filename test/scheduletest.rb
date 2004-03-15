8bff81e5b1f62654cb20db5c67debf14df38fb4f
Matthew Lipper <mlipper@gmail.com>
2004-03-15 22:02:58 +0000

    Begining of test case refactor to support named TE's.

--- test/scheduletest.rb
+++ test/scheduletest.rb
@@ -18,12 +18,24 @@ class ScheduleTest < Test::Unit::TestCase
   end
 
   def test_add
-    schedule = Schedule.new
-    elmo_party = Event.new("Elmo's Birthday Party",schedule)
-    schedule.add(elmo_party,RangeEachDayTE.new(10,00,16,00))
-    assert(schedule.is_occurring?(elmo_party, TimePoint.new(2004,2,1,11,06)))
-    assert(schedule.is_occurring?(!elmo_party, TimePoint.new(2004,2,1,9,06)))
-    assert(elmo_party.schedule==schedule)
+    elmo_party_schedule = Schedule.new
+    elmo_party = Event.new("Elmo's Birthday Party",elmo_party_schedule)
+    elmo_party_schedule.add(elmo_party,@elmo_party_te)
+    assert(elmo_party_schedule.is_occurring?(elmo_party, TimePoint.new(2004,2,1,11,06)))
+    assert(elmo_party_schedule.is_occurring?(!elmo_party, TimePoint.new(2004,2,1,9,06)))
+    assert(elmo_party.schedule==elmo_party_schedule)
+
+    tv_schedule = Schedule.new
+    elmos_world = Event.new("Elmo's World",tv_schedule)
+
+    p @sesame_street_broadcast_te
+    #schedule.add(
+
   end
 
+  def setup
+    #~ @elmo_party = Event.new("Elmo's Birthday Party")
+    @elmo_party_te = RangeEachDayTE.new(10,00,16,00)
+    @sesame_street_broadcast_te = IntersectionTE.new.add(RangeEachWeekTE.new(Monday,Friday)).add(RangeEachDayTE.new(9,00,10,00))
+  end
 end
