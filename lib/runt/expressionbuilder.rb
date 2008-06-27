8a355239718e195002f78a96ae0865473f1f774a
Matthew Lipper <mlipper@gmail.com>
2008-06-27 16:08:54 +0000

    Added ExpressionBuilder class for creating expressions using more human-friendly syntax

---
+++ lib/runt/expressionbuilder.rb
@@ -0,0 +1,72 @@
+#
+# Convenience class for building temporal expressions in a more 
+# human-friendly way. Used in conjunction with shortcuts defined in the 
+# sugar.rb file, this allows one to create expressions like the following:
+#   
+#   b = ExpressionBuilder.new
+#   expr = b.define do
+#     occurs daily_8_30am_to_9_45am
+#     on tuesday
+#     possibly wednesday
+#   end
+#
+# This equivalent to:
+#
+#   expr = REDay.new(8,30,9,45) & DIWeek.new(Tuesday) | DIWeek.new(Wednesday)
+#
+# ExpressionBuilder creates expressions by evaluating a block passed to the 
+# #define method. From inside the block, methods occurs, on, every, possibly,
+# and maybe can be called with a temporal expression which will be added to
+# a composite expression as follows:
+#  
+#
+require 'runt'
+
+class ExpressionBuilder
+
+  include Runt
+
+  attr_accessor :ctx
+
+  def initialize
+    @ctx = nil
+  end
+
+  def define(&block)
+    instance_eval(&block)
+  end
+  
+  def on(expr)
+    add(expr, :&)
+  end
+
+  def add(expr, op)
+    @ctx ||= expr
+    @ctx = @ctx.send(op, expr) unless @ctx == expr
+    @ctx # explicit return, previous line may not execute
+  end
+
+  def except(expr)
+    add(expr, :-)
+  end
+  
+  def possibly(expr)
+    add(expr, :|)
+  end
+
+  alias_method :every, :on
+  alias_method :occurs, :on
+  alias_method :maybe, :possibly
+end
+
+if __FILE__ == $0
+  builder = ExpressionBuilder.new
+  expr = builder.define do
+    occurs daily_11_00am_to_3_00pm
+    on monday 
+    possibly tuesday
+    except last_monday
+  end
+  puts expr
+end
+
---
+++ test/expressionbuildertest.rb
@@ -0,0 +1,54 @@
+#!/usr/bin/env ruby
+
+require 'test/unit'
+require 'runt'
+require 'runt/expressionbuilder'
+
+class ExpressionBuilderTest < Test::Unit::TestCase
+
+  def setup
+    @builder = ExpressionBuilder.new
+  end
+
+  def test_define_should_instance_eval_a_block
+    @builder.define do
+      @ctx = "meow"
+    end
+    assert_equal "meow", @builder.ctx, "Expected instance variable @ctx to be set by block"  
+  end
+  
+  def test_add_should_initialize_empty_ctx_with_expression
+    result = @builder.add('expr',:to_s)
+    assert_equal 'expr', result, "Result should equal string given to add method"
+    assert_equal 'expr', @builder.ctx, "Builder context should equal result"
+  end
+
+  def test_add_should_send_op_to_ctx_with_expression
+    @builder.add('abc',:to_s)
+    result = @builder.add('def',:concat)
+    assert_equal 'abcdef', result, "Result should equal concatenated strings given to add method"
+    assert_equal 'abcdef', @builder.ctx, "Builder context should equal result"
+  end
+
+  def test_on_should_call_add_with_expression_and_ampersand
+    @builder.add(1,:to_s)
+    result = @builder.on(3) # result = 1 & 3
+    assert_equal 1, result, "Result should equal 1 == 1 & 3"
+    assert_equal 1, @builder.ctx, "Builder context should equal result"
+  end
+
+  def test_except_should_call_add_with_expression_and_minus
+    @builder.add(1,:to_s)
+    result = @builder.except(3) # result = 1 - 3
+    assert_equal -2, result, "Result should equal -2 == 1 - 3"
+    assert_equal -2, @builder.ctx, "Builder context should equal result"
+  end
+
+  def test_possibly_should_call_add_with_expression_and_pipe
+    @builder.add(1, :to_s)
+    result = @builder.possibly(2) # result = 1 | 2
+    assert_equal 3, result, "Result should equal 3 == 1 | 2"
+    assert_equal 3, @builder.ctx, "Builder context should equal result"
+  end
+
+end
