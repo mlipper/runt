d1d453d9c67930c5441aa9af7fb8155cd8a97861
Matthew Lipper <mlipper@gmail.com>
2013-04-26 12:30:16 -0400

    Fix performance of DateRange test on >= Ruby 1.9
    Temporary fix for the performance of the DateRange tests when running
    on Ruby 1.9 or above. The sub_range test was using second precision.
    It has been changed to use minute precision which is not nearly so slow.

--- test/daterangetest.rb
+++ test/daterangetest.rb
@@ -10,8 +10,8 @@ class DateRangeTest < MiniTest::Unit::TestCase
   include Runt
 
   def test_sub_range
-    r_start = PDate.sec(2004,2,29,16,24,12)
-    r_end = PDate.sec(2004,3,2,4,22,58)
+    r_start = PDate.min(2004,2,29,16,24)
+    r_end = PDate.min(2004,3,2,4,22)
     range = DateRange.new(r_start,r_end)
     assert(range.min==r_start)
     assert(range.max==r_end)
